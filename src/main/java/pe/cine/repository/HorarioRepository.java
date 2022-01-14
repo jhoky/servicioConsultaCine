cac1a9ea2743c6eb03c1da7dd094b11c2068b076
Angel <93287365+TheSpecialOne22@users.noreply.github.com>
2022-01-14 14:32:00 -0500

    Update HorarioRepository.java

--- src/main/java/pe/cine/repository/HorarioRepository.java
+++ src/main/java/pe/cine/repository/HorarioRepository.java
@@ -1,5 +1,6 @@
 package pe.cine.repository;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +16,8 @@ import pe.cine.model.Horario;
 public interface HorarioRepository extends JpaRepository<Horario, Integer> {
 	
 	
-	@Query(value = "select h.horario_id, h.cine_id, h.estado,h.fecha,h.hora_inicio,h.fk_pelicula_id from pelicula p inner join horario h on p.pelicula_id = h.fk_pelicula_id where p.pelicula_id = ?1  and h.cine_id = ?2",nativeQuery = true)
-	ArrayList<Horario> buscarFiltro(int idpel, int idcine,String fech);
+	@Query(value="SELECT * FROM Horario h WHERE h.cine_id= :cine AND h.fk_pelicula_id= :pelicula AND h.fecha= :fech",nativeQuery = true)
+	public List<Horario> buscarFiltro(@Param("cine") int cine,@Param("pelicula") int pelicula, @Param("fech") Date fech);
 	
 	@Query(value = "select h.horario_id, h.cine_id, h.estado,h.fecha,h.hora_inicio,h.fk_pelicula_id from pelicula p inner join horario h on p.pelicula_id = h.fk_pelicula_id where p.pelicula_id = ?1  and h.cine_id = ?2",nativeQuery = true)
 	ArrayList<Horario> listahorariosporPelicula(int idpel, int idcine);
