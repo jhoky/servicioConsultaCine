6d3d710c03c1c1ecf604d9679db667b5afc97d3d
Angel <93287365+TheSpecialOne22@users.noreply.github.com>
2022-01-14 04:25:27 -0500

    Update HorarioRepository.java

--- src/main/java/pe/cine/repository/HorarioRepository.java
+++ src/main/java/pe/cine/repository/HorarioRepository.java
@@ -1,5 +1,6 @@
 package pe.cine.repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,9 @@ public interface HorarioRepository extends JpaRepository<Horario, Integer> {
 	
 	@Query(value = "select h.horario_id, h.cine_id, h.estado,h.fecha,h.hora_inicio,h.fk_pelicula_id from pelicula p inner join horario h on p.pelicula_id = h.fk_pelicula_id where p.pelicula_id = ?1  and h.cine_id = ?2",nativeQuery = true)
 	ArrayList<Horario> listahorariosporPelicula(int idpel, int idcine);
+	
+	@Query(value="SELECT hora_inicio FROM horario WHERE cine_id= :cine AND fk_pelicula_id= :pelicula AND fecha= :fecha AND hora_inicio= :hora", nativeQuery=true)
+	List<?> listarHoras(@Param("cine") int cine, @Param("pelicula") int pelicula, @Param("fecha") LocalDate fecha, @Param("hora") String hora);
+
 
 }
