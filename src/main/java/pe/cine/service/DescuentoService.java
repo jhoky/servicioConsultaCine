7c9daf1e0fb526eb48d839cbfdc04ccad537cb2a
Angel <93287365+TheSpecialOne22@users.noreply.github.com>
2022-01-14 20:40:33 -0500

    Add files via upload

---
+++ src/main/java/pe/cine/service/DescuentoService.java
@@ -0,0 +1,42 @@
+package pe.cine.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.cine.model.CodigoDescuento;
+import pe.cine.repository.DescuentoRepository;
+
+
+@Service
+@Transactional
+public class DescuentoService {
+	
+	@Autowired
+	DescuentoRepository repository;
+	
+	public List<CodigoDescuento> list(){
+        return repository.findAll();
+    }
+
+    public Optional<CodigoDescuento> getOne(int id){
+        return repository.findById(id);
+    }
+
+    public void save(CodigoDescuento codigoDescuento){
+    	repository.save(codigoDescuento);
+    }
+
+    public void delete(int id){
+    	repository.deleteById(id);
+    }
+
+    public boolean existsById(int id){
+        return repository.existsById(id);
+    }
+
+}
