5f69e85a85ac0614d8d4a204b5ffc7ae745021bf
jhoky <86019853+jhoky@users.noreply.github.com>
2022-01-19 18:21:46 -0500

    actualizacion descuento repository

--- src/main/java/pe/cine/repository/DescuentoRepository.java
+++ src/main/java/pe/cine/repository/DescuentoRepository.java
@@ -8,4 +8,8 @@ import pe.cine.model.CodigoDescuento;
 @Repository
 public interface DescuentoRepository extends JpaRepository<CodigoDescuento, Integer>{
 
+  @Query(value = "select cd.descuento_id,cd.nr_descuento,cd.usuario_id from usuario u inner join codigo_descuento cd on u.usuario_id = cd.usuario_id where u.email = ?1",nativeQuery = true)
+    CodigoDescuento buscarcodigodescuento(String email);
+
+  
 }
