080eb25c7252a6c158b2fd2f80150425472d3fa7
jhoky <86019853+jhoky@users.noreply.github.com>
2022-01-22 23:42:35 -0500

    actualizacion DescuentoRepository

--- src/main/java/pe/cine/repository/DescuentoRepository.java
+++ src/main/java/pe/cine/repository/DescuentoRepository.java
@@ -1,6 +1,8 @@
 package pe.cine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import pe.cine.model.CodigoDescuento;
