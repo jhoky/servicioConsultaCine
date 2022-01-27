d80ae85f6a12aa999a3dc68926fcfdbe7ae2b768
Angel <93287365+TheSpecialOne22@users.noreply.github.com>
2022-01-27 05:37:11 -0500

    Update DirectorRepository.java

--- src/main/java/pe/cine/repository/DirectorRepository.java
+++ src/main/java/pe/cine/repository/DirectorRepository.java
@@ -1,8 +1,10 @@
 package pe.cine.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.cine.model.Director;
@@ -12,5 +14,7 @@ public interface DirectorRepository extends JpaRepository<Director, Integer> {
 	
 	Optional<Director> findByNombre(String nombre);
     boolean existsByNombre(String nombre);
+	@Query(value="SELECT * FROM director ORDER BY director_id DESC", nativeQuery=true)
+	List<Director> listDesc();
 
 }
