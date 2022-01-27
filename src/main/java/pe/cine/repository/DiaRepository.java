7009bbd055d724a17079cbab91fc5ae4e2554f76
Angel <93287365+TheSpecialOne22@users.noreply.github.com>
2022-01-27 04:50:53 -0500

    Update DiaRepository.java

--- src/main/java/pe/cine/repository/DiaRepository.java
+++ src/main/java/pe/cine/repository/DiaRepository.java
@@ -1,8 +1,10 @@
 package pe.cine.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.cine.model.Dia;
@@ -12,5 +14,7 @@ public interface DiaRepository extends JpaRepository<Dia, Integer> {
 	
 	Optional<Dia> findByNombre(String nombre);
     boolean existsByNombre(String nombre);
+	@Query(value = "SELECT * FROM dia ORDER BY dia_id ASC",nativeQuery = true)
+    List<Dia> listAsc();
 
 }
